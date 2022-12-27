9f58d9ad0aa9472b7580668ab674b2a5def00116
leejungsik <godsider@naver.com>
2022-12-27 16:20:18 +0900

    클래스 연습 커밋

--- class2.py
+++ class2.py
@@ -2,11 +2,18 @@ class Person:
     #초기화메서드
     def __init__(self):
         self.name = "default name"
+        self.title = "New New"
     def print(self):
         print("My name is {0}".format(self.name))
 
 p1 = Person()
 p2 = Person()
 
+p1.name = "전우치"
+
 p1.print()
 p2.print()
+
+# Person.title = "New Title"
+print(p1.title)
+print(Person.title)
